8ba6af7eff26b527bf74ab26d7dbf690eb96d819
Tim Charteris <tim.charterii@gmail.com>
2024-12-31 15:12:12 +0000

    checkSudoku logic for seeing if basic rules are broken

--- index.js
+++ index.js
@@ -1,24 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
     const gridElement = document.getElementById("grid");
     let grid = [];
-  
-    for (let i = 0; i < 9; i++) {
-      let row = [];
-      for (let j = 0; j < 9; j++) {
-        let cell = document.createElement("input");
-        cell.type = "number";
-        cell.classList.add("cell");
-        cell.id = `${i}/${j}`;
-        row.push(cell);
-        gridElement.appendChild(cell);
-      }
-      grid.push(row);
-    }
-
+    
     function getBoxIndex(row, col) {
         return Math.floor(row / 3) * 3 + Math.floor(col / 3);
+    }
+
+    for (let r = 0; r < 9; r++) {
+        let row = [];
+        for (let c = 0; c < 9; c++) {
+            let cell = document.createElement("input");
+            cell.type = "number";
+            cell.classList.add("cell");
+            cell.row = r + 1;
+            cell.col = c + 1;
+            cell.id = `${cell.row}/${cell.col}`
+            cell.box = getBoxIndex(r, c);
+            row.push(cell);
+            gridElement.appendChild(cell);
+        }
+        grid.push(row);
+    }
+
+    document.getElementById("checkButton").addEventListener("click", function() {
+        checkSudoku(grid);
+    });
+});
+
+function checkSudoku(grid) {
+    let rows = Array(9).fill().map(() => []);
+    let cols = Array(9).fill().map(() => []);
+    let boxes = Array(9).fill().map(() => []);
+    let isValid = true;
+
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            let cell = grid[r][c];
+            let value = cell.value;
+            if (value) {
+                if (rows[r].includes(value) || cols[c].includes(value) || boxes[cell.box].includes(value)) {
+                    isValid = false;
+                    return
+                } else {
+                    rows[r].push(value);
+                    cols[c].push(value);
+                    boxes[cell.box].push(value);
+                }
+            }
+        }
       }
-    
-    console.log(getBoxIndex(3, 8));
-  });
-  
+      return isValid
+}
