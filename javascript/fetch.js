0fbb324a6e757a1f61c3f58dc9be80d1586d18ab
Tim Charteris <tim.charterii@gmail.com>
2025-01-07 15:45:09 +0000

    configure for github pages to call local backend

--- javascript/fetch.js
+++ javascript/fetch.js
@@ -1,14 +1,10 @@
 import { populateGrid } from './populate.js';
 import { state } from './state.js';
 
-const BASE_URL =
-    window.location.hostname === 'localhost'
-        ? 'http://127.0.0.1:5000' // Local backend
-        : 'https://killer-sudoku.onrender.com'; // Deployed backend
 
 const handleSolveButtonClick = async () => {
     try {
-        const response = await fetch(`${BASE_URL}/solve`, {
+        const response = await fetch(`http://127.0.0.1:5000/solve`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ grid: state.grid, boxes: state.boxes }),
